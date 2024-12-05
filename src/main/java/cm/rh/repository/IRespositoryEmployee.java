7d7df903d5ac5e31b7c2973884e0e18054bedba4
ChristianMallo <118746425+ChristianMallo@users.noreply.github.com>
2024-12-05 13:04:47 +0100

    Troubleshooting local and remote repository problems

--- src/main/java/cm/rh/repository/IRespositoryEmployee.java
+++
@@ -1,9 +0,0 @@
-package cm.rh.repository;
-
-import cm.rh.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRespositoryEmployee extends JpaRepository<Employee, Integer> {
-}
